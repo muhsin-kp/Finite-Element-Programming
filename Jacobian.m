05372d023162b09da311b79479d2ae225d3878f6
Muhsinkp <37369718+Muhsinkp@users.noreply.github.com>
2022-05-27 20:15:26 +0530

    Delete Jacobian.m

--- Jacobian.m
+++
@@ -1,5 +0,0 @@
-function [J,derivativeShapeFunction_x_y]=Jacobian(nodalCoordinateMat,derivativeShapeFunction_si_eta)
-
-            J=derivativeShapeFunction_si_eta*nodalCoordinateMat;
-            derivativeShapeFunction_x_y=J\derivativeShapeFunction_si_eta;
-end
